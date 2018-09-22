bc5283c4785e1b7c913262d31d741252eed3bfc9
Casey Schaufler <casey@schaufler-ca.com>
2018-09-21 17:19:11 -0700

    UPSTREAM: SELinux: Abstract use of inode security blob
    Don't use the inode->i_security pointer directly.
    Provide a helper function that provides the security blob pointer.
    
    Change-Id: Ic4d5997d2dfdbf1c25d6480f83273bd7c94b2250
    Signed-off-by: Casey Schaufler <casey@schaufler-ca.com>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Kees Cook <keescook@chromium.org>

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -1408,7 +1408,7 @@ static int sel_make_bools(struct selinux_fs_info *fsi)
 		if (len >= PAGE_SIZE)
 			goto out;
 
-		isec = (struct inode_security_struct *)inode->i_security;
+		isec = selinux_inode(inode);
 		ret = security_genfs_sid(fsi->state, "selinuxfs", page,
 					 SECCLASS_FILE, &sid);
 		if (ret) {
@@ -2029,7 +2029,7 @@ static int sel_fill_super(struct super_block *sb, void *data, int silent)
 		goto err;
 
 	inode->i_ino = ++fsi->last_ino;
-	isec = (struct inode_security_struct *)inode->i_security;
+	isec = selinux_inode(inode);
 	isec->sid = SECINITSID_DEVNULL;
 	isec->sclass = SECCLASS_CHR_FILE;
 	isec->initialized = LABEL_INITIALIZED;
